35d57c29ac63030d3760374009079b24a599b911
mina987 <m9.agrest@gmail.com>
2026-04-22 14:12:55 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
-﻿dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true
+if errorlevel 1 exit /b 1
+
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "innoSetup.iss"
+if errorlevel 1 exit /b 1
 
 pause
